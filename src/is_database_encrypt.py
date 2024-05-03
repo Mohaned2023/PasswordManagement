dbe2e5ea951879af504cb75faf104c3e9ffa59f7
Mohaned2023 <shrhanmhnd15@gmail.com>
2024-05-03 12:47:55 +0300

    Fix the checking condition

--- src/is_database_encrypt.py
+++ src/is_database_encrypt.py
@@ -1,12 +1,12 @@
 def is_database_encrypt(DATABASE_PATH:str, DATABASE_FILE_JSON:str, DATABASE_FILE_X:str , listdir) -> bool : # check if the database is encrypted.. 
     # Get all files in `DataBase` folder.. 
     files: list[str] = listdir(DATABASE_PATH)
-    # If the file is not encrypted.. 
-    if DATABASE_FILE_JSON in files and DATABASE_FILE_X not in files:
-        return False
     # If the file is not encrypted and not decrypted.. 
-    elif DATABASE_FILE_JSON in files and DATABASE_FILE_X in files:
+    if DATABASE_FILE_JSON in files and DATABASE_FILE_X in files:
         raise Exception('Error: Database is not good..!') 
     # If the database is encrypted.. 
-    else :
+    elif DATABASE_FILE_X in files :
         return True
+    # If the file is not encrypted..
+    else :
+        return False
